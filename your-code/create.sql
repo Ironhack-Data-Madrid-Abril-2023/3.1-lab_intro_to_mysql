de4c3365dbc0dd31a0682bd4ef25f3c90eb73439
Carlos Palomares <carlospalomaresgarcia@gmail.com>
2023-04-24 18:19:15 +0200

    [Carlos Palomares] (3.1-lab_intro_to_mysql)

---
+++ your-code/create.sql
@@ -0,0 +1,110 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema 3.1-lab_intro_to_mysql
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema 3.1-lab_intro_to_mysql
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `3.1-lab_intro_to_mysql` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
+USE `3.1-lab_intro_to_mysql` ;
+
+-- -----------------------------------------------------
+-- Table `3.1-lab_intro_to_mysql`.`salesperson`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `3.1-lab_intro_to_mysql`.`salesperson` (
+  `idstaff` VARCHAR(45) NOT NULL,
+  `staff ID` INT NOT NULL,
+  `name` VARCHAR(45) NOT NULL,
+  `store` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`idstaff`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `3.1-lab_intro_to_mysql`.`customer`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `3.1-lab_intro_to_mysql`.`customer` (
+  `customer_id` INT NOT NULL,
+  `customer_number` VARCHAR(70) NULL,
+  `name` VARCHAR(70) NOT NULL,
+  `phone_number` VARCHAR(30) NOT NULL,
+  `email` VARCHAR(45) NOT NULL,
+  `address` VARCHAR(100) NOT NULL,
+  `city` VARCHAR(70) NOT NULL,
+  `state/province` VARCHAR(20) NOT NULL,
+  `country` VARCHAR(20) NOT NULL,
+  `zip/postal code` VARCHAR(70) NOT NULL,
+  PRIMARY KEY (`customer_id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `3.1-lab_intro_to_mysql`.`invoice`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `3.1-lab_intro_to_mysql`.`invoice` (
+  `Invoice_ID` INT NOT NULL AUTO_INCREMENT,
+  `Invoice_number` VARCHAR(45) NOT NULL,
+  `date` DATETIME NOT NULL,
+  `car` VARCHAR(45) NOT NULL,
+  `customer` VARCHAR(100) NOT NULL,
+  `salesperson` VARCHAR(70) NOT NULL,
+  `salesperson_idstaff` VARCHAR(45) NOT NULL,
+  `customer_customer_id` INT NOT NULL,
+  PRIMARY KEY (`Invoice_ID`, `salesperson_idstaff`, `customer_customer_id`),
+  INDEX `fk_invoice_salesperson_idx` (`salesperson_idstaff` ASC) VISIBLE,
+  INDEX `fk_invoice_customer1_idx` (`customer_customer_id` ASC) VISIBLE,
+  CONSTRAINT `fk_invoice_salesperson`
+    FOREIGN KEY (`salesperson_idstaff`)
+    REFERENCES `3.1-lab_intro_to_mysql`.`salesperson` (`idstaff`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_invoice_customer1`
+    FOREIGN KEY (`customer_customer_id`)
+    REFERENCES `3.1-lab_intro_to_mysql`.`customer` (`customer_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `3.1-lab_intro_to_mysql`.`cars`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `3.1-lab_intro_to_mysql`.`cars` (
+  `ID_cars` VARCHAR(45) NOT NULL,
+  `VIN` VARCHAR(25) NOT NULL,
+  `manufacturer` VARCHAR(45) NOT NULL,
+  `model` VARCHAR(45) NOT NULL,
+  `year` INT NOT NULL,
+  `color` VARCHAR(20) NOT NULL,
+  `invoice_Invoice_ID` INT NOT NULL,
+  `invoice_salesperson_idstaff` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`ID_cars`, `invoice_Invoice_ID`, `invoice_salesperson_idstaff`),
+  INDEX `fk_cars_invoice1_idx` (`invoice_Invoice_ID` ASC, `invoice_salesperson_idstaff` ASC) VISIBLE,
+  CONSTRAINT `fk_cars_invoice1`
+    FOREIGN KEY (`invoice_Invoice_ID` , `invoice_salesperson_idstaff`)
+    REFERENCES `3.1-lab_intro_to_mysql`.`invoice` (`Invoice_ID` , `salesperson_idstaff`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
