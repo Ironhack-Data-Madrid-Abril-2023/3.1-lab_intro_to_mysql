a9ee4cbfffbe0c80850d825bdf2cf52ee79f3c3e
Giotto Millan <giottomillan@gmail.com>
2023-04-24 17:25:44 +0200

    Done

---
+++ delete.sql.txt
@@ -0,0 +1,3 @@
+USE lab1;
+
+delete from Cars where VIN = 'DAM41UDN3CHU2WVF6';
---
+++ your-code/update.sql
@@ -0,0 +1,11 @@
+update Customers
+set Email = 'ppicasso@gmail.com'
+where id = 0;
+
+update Customers
+set Email = 'lamarr_hd@hollywood.com'
+where id = 1;
+
+update Customers
+set Email = 'hello@nasa.gov'
+where id = 2;
