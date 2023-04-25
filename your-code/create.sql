8f23510f6e8fcfaf1bb22d4e3931d6c39a20f21a
David Aparicio <apariciofernandez.d@gmail.com>
2023-04-25 11:54:12 +0200

    3.1-lab_intro_to_mysql_CHALLENGE1_v2

--- your-code/create.sql
+++ your-code/create.sql
@@ -1,5 +1,3 @@
-USE lab_mysql;
-
 -- MySQL Workbench Forward Engineering
 
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
@@ -13,8 +11,7 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- -----------------------------------------------------
 -- Schema lab_mysql
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `lab_mysql` DEFAULT CHARACTER SET utf8 ;
-
+CREATE SCHEMA IF NOT EXISTS `lab_mysql` DEFAULT CHARACTER SET utf8mb3 ;
 USE `lab_mysql` ;
 
 -- -----------------------------------------------------
@@ -23,12 +20,13 @@ USE `lab_mysql` ;
 CREATE TABLE IF NOT EXISTS `lab_mysql`.`CARS` (
   `idCARS` INT NOT NULL,
   `VIN` INT NULL,
-  `manufacturer` VARCHAR(45) NULL,
-  `model` VARCHAR(45) NULL,
-  `year` YEAR NULL,
-  `color` TEXT NULL,
+  `manufacturer` VARCHAR(45) NULL DEFAULT NULL,
+  `model` VARCHAR(45) NULL DEFAULT NULL,
+  `year` YEAR NULL DEFAULT NULL,
+  `color` TEXT NULL DEFAULT NULL,
   PRIMARY KEY (`idCARS`))
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
 
 
 -- -----------------------------------------------------
@@ -36,17 +34,18 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `lab_mysql`.`CUSTOMERS` (
   `idCUSTOMERS` INT NOT NULL,
-  `ID` INT NULL,
-  `name` TEXT NULL,
-  `phone number` INT NULL,
-  `email` VARCHAR(45) NULL,
-  `adress` VARCHAR(45) NULL,
-  `city` TEXT NULL,
-  `state/province` TEXT NULL,
-  `country` TEXT NULL,
-  `zip/postal code` INT NULL,
+  `ID` INT NULL DEFAULT NULL,
+  `name` TEXT NULL DEFAULT NULL,
+  `phone number` INT NULL DEFAULT NULL,
+  `email` VARCHAR(45) NULL DEFAULT NULL,
+  `adress` VARCHAR(45) NULL DEFAULT NULL,
+  `city` TEXT NULL DEFAULT NULL,
+  `state/province` TEXT NULL DEFAULT NULL,
+  `country` TEXT NULL DEFAULT NULL,
+  `zip/postal code` INT NULL DEFAULT NULL,
   PRIMARY KEY (`idCUSTOMERS`))
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
 
 
 -- -----------------------------------------------------
@@ -54,11 +53,12 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `lab_mysql`.`SALESPERSONS` (
   `idSALESPERSONS` INT NOT NULL,
-  `ID` INT NULL,
-  `name` TEXT NULL,
-  `store` VARCHAR(45) NULL,
+  `ID` INT NULL DEFAULT NULL,
+  `name` TEXT NULL DEFAULT NULL,
+  `store` VARCHAR(45) NULL DEFAULT NULL,
   PRIMARY KEY (`idSALESPERSONS`))
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
 
 
 -- -----------------------------------------------------
@@ -66,53 +66,34 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `lab_mysql`.`INVOICES` (
   `idINVOICES` INT NOT NULL,
-  `ID` INT NULL,
-  `number` INT NULL,
-  `date` DATE NULL,
-  `car` VARCHAR(45) NULL,
-  `customer` TEXT NULL,
-  `salesperson` TEXT NULL,
-  PRIMARY KEY (`idINVOICES`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `lab_mysql`.`SALES`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `lab_mysql`.`SALES` (
-  `idSALES` INT NOT NULL,
-  `CARS_idCARS` INT NOT NULL,
+  `ID` INT NULL DEFAULT NULL,
+  `number` INT NULL DEFAULT NULL,
+  `date` DATE NULL DEFAULT NULL,
   `CUSTOMERS_idCUSTOMERS` INT NOT NULL,
   `SALESPERSONS_idSALESPERSONS` INT NOT NULL,
-  `INVOICES_idINVOICES` INT NOT NULL,
-  `units` INT NOT NULL,
-  `price` DECIMAL NULL,
-  PRIMARY KEY (`idSALES`, `CARS_idCARS`, `CUSTOMERS_idCUSTOMERS`, `SALESPERSONS_idSALESPERSONS`, `INVOICES_idINVOICES`),
-  INDEX `fk_SALES_CARS_idx` (`CARS_idCARS` ASC) VISIBLE,
-  INDEX `fk_SALES_CUSTOMERS1_idx` (`CUSTOMERS_idCUSTOMERS` ASC) VISIBLE,
-  INDEX `fk_SALES_SALESPERSONS1_idx` (`SALESPERSONS_idSALESPERSONS` ASC) VISIBLE,
-  INDEX `fk_SALES_INVOICES1_idx` (`INVOICES_idINVOICES` ASC) VISIBLE,
-  CONSTRAINT `fk_SALES_CARS`
-    FOREIGN KEY (`CARS_idCARS`)
-    REFERENCES `mydb`.`CARS` (`idCARS`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_SALES_CUSTOMERS1`
+  `CARS_idCARS` INT NOT NULL,
+  PRIMARY KEY (`idINVOICES`, `CUSTOMERS_idCUSTOMERS`, `SALESPERSONS_idSALESPERSONS`, `CARS_idCARS`),
+  INDEX `fk_INVOICES_CUSTOMERS_idx` (`CUSTOMERS_idCUSTOMERS` ASC) VISIBLE,
+  INDEX `fk_INVOICES_SALESPERSONS1_idx` (`SALESPERSONS_idSALESPERSONS` ASC) VISIBLE,
+  INDEX `fk_INVOICES_CARS1_idx` (`CARS_idCARS` ASC) VISIBLE,
+  CONSTRAINT `fk_INVOICES_CUSTOMERS`
     FOREIGN KEY (`CUSTOMERS_idCUSTOMERS`)
-    REFERENCES `mydb`.`CUSTOMERS` (`idCUSTOMERS`)
+    REFERENCES `lab_mysql`.`CUSTOMERS` (`idCUSTOMERS`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_SALES_SALESPERSONS1`
+  CONSTRAINT `fk_INVOICES_SALESPERSONS1`
     FOREIGN KEY (`SALESPERSONS_idSALESPERSONS`)
-    REFERENCES `mydb`.`SALESPERSONS` (`idSALESPERSONS`)
+    REFERENCES `lab_mysql`.`SALESPERSONS` (`idSALESPERSONS`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_SALES_INVOICES1`
-    FOREIGN KEY (`INVOICES_idINVOICES`)
-    REFERENCES `mydb`.`INVOICES` (`idINVOICES`)
+  CONSTRAINT `fk_INVOICES_CARS1`
+    FOREIGN KEY (`CARS_idCARS`)
+    REFERENCES `lab_mysql`.`CARS` (`idCARS`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
+
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
