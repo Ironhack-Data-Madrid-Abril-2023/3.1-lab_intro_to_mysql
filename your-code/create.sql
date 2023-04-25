f653b6da5e10ce2f3b03da254f50a190eb9abdfa
JesusGuardiaRamirez <guardiaramirez.jesus@gmail.com>
2023-04-25 15:53:01 +0200

    completed

---
+++ your-code/create.sql
@@ -0,0 +1,100 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
+USE `mydb` ;
+
+-- -----------------------------------------------------
+-- Table `mydb`.`CARS`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`CARS` (
+  `ID` INT NOT NULL AUTO_INCREMENT,
+  `VIN` VARCHAR(45) NOT NULL,
+  `MANUFACTURER` VARCHAR(45) NOT NULL,
+  `MODEL` VARCHAR(45) NOT NULL,
+  `YEAR` YEAR(4) NOT NULL,
+  `COLOR` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE INDEX `ID_UNIQUE` (`ID` ASC) VISIBLE)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`CUSTOMER`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`CUSTOMER` (
+  `ID` INT NOT NULL AUTO_INCREMENT,
+  `CUSTOID` VARCHAR(45) NOT NULL,
+  `NAME` VARCHAR(45) NOT NULL,
+  `PHONE` VARCHAR(45) NOT NULL,
+  `EMAIL` VARCHAR(45) NOT NULL,
+  `ADDRESS` VARCHAR(45) NOT NULL,
+  `CITY` VARCHAR(45) NOT NULL,
+  `SATATE` VARCHAR(45) NOT NULL,
+  `COUNTRY` VARCHAR(45) NOT NULL,
+  `ZIPCODE` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE INDEX `ID_UNIQUE` (`ID` ASC) VISIBLE)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`SALESPERSON`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`SALESPERSON` (
+  `ID` INT NOT NULL AUTO_INCREMENT,
+  `STAFFID` VARCHAR(45) NOT NULL,
+  `NAME` VARCHAR(45) NOT NULL,
+  `STORE` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE INDEX `ID_UNIQUE` (`ID` ASC) VISIBLE)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`INVOICE`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`INVOICE` (
+  `ID` INT NOT NULL AUTO_INCREMENT,
+  `INVOICE_NO` VARCHAR(45) NOT NULL,
+  `DATE` VARCHAR(45) NOT NULL,
+  `SALESPERSON_ID` INT NOT NULL,
+  `CUSTOMER_ID` INT NOT NULL,
+  `CARS_ID` INT NOT NULL,
+  PRIMARY KEY (`ID`, `SALESPERSON_ID`, `CUSTOMER_ID`, `CARS_ID`),
+  UNIQUE INDEX `ID_UNIQUE` (`ID` ASC) VISIBLE,
+  INDEX `fk_INVOICE_SALESPERSON_idx` (`SALESPERSON_ID` ASC) VISIBLE,
+  INDEX `fk_INVOICE_CUSTOMER1_idx` (`CUSTOMER_ID` ASC) VISIBLE,
+  INDEX `fk_INVOICE_CARS1_idx` (`CARS_ID` ASC) VISIBLE,
+  CONSTRAINT `fk_INVOICE_SALESPERSON`
+    FOREIGN KEY (`SALESPERSON_ID`)
+    REFERENCES `mydb`.`SALESPERSON` (`ID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_INVOICE_CUSTOMER1`
+    FOREIGN KEY (`CUSTOMER_ID`)
+    REFERENCES `mydb`.`CUSTOMER` (`ID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_INVOICE_CARS1`
+    FOREIGN KEY (`CARS_ID`)
+    REFERENCES `mydb`.`CARS` (`ID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
