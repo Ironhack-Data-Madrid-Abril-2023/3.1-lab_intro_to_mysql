5e56c686cf9c3952db1addef805d76b83aa7e1d4
Javier Rodríguez <javirodrisanz@gmail.com>
2023-04-25 12:11:59 +0200

    subida 2

--- your-code/seeding.sql
+++ your-code/seeding.sql
@@ -1,22 +1,32 @@
-INSERT into cars (VIN, manufacturer, model, year, color)
-	values ("3K096I98581DHSNUP", "Volkswagen", "Tiguan", "2019", "Blue"),
-			("ZM8G7BEUQZ97IH46V", "Peugeot", "Rifter", "2019", "Red"),
-            ("RKXVNNIHLVVZOUB4M","Ford","Fusion", "2018", "White"),
-            ("HKNDGS7CU31E9Z7JW","Toyota", "RAV4", "2018","Silver"),
-			("DAM41UDN3CHU2WVF6", "Volvo", "V60", "2019", "Gray"),
-            ("DAM41UDN3CHU2WVF6", "Volvo", "Cross Country", "2019", "Gray");
+INSERT INTO cars (id_car, VIN, manufacturer, model, year, color)
+VALUES 
+			(1,"3K096I98581DHSNUP", "Volkswagen", "Tiguan", "2019", "Blue"),
+			(2, "ZM8G7BEUQZ97IH46V", "Peugeot", "Rifter", "2019", "Red"),
+			(3, "RKXVNNIHLVVZOUB4M", "Ford", "Fusion", "2018", "White"),
+			(4, "HKNDGS7CU31E9Z7JW", "Toyota", "RAV4", "2018", "Silver"),
+			(5, "DAM41UDN3CHU2WVF6", "Volvo", "V60", "2019", "Gray"),
+			(6, "DAM41UDN3CHU2WVF6", "Volvo", "Cross Country", "2019", "Gray");
 
-INSERT into customers (id, id_customer,name,phone,email,address,city,state/province,country,postal_code)
-			("0", "10001", "Pablo Picasso", "+34 636 17 63 82", "Paseo de la Chopera, 14", "Madrid", "Madrid", "Spain", "28045"),
-			("1", "20001", "Hedy Lamarr", "+43 1 514442250", "Weiglgasse 10	Viena", "Vienna", "Austria", "1150"),
-			("2", "30001", "Katherine Johnson", "+1 202 358 0000 300", "E St SW	Washington", "DC Washington", "United States", "20546");
+ALTER TABLE Customers CHANGE COLUMN `postal-code` postal_code VARCHAR(50);
 
-INSERT into salespersons (id, id_salesperson, name, store)
-			("0","00001","Petey Cruiser","Madrid"),
-			("1","00002","Anna Sthesia", "Barcelona"),
-			("2","00003","Paul Molive","Berlin"),
-			("3","00004","Gail Forcewind","Paris");
-INSERT into invoices (ID, invoice, number, date, car, Customer, Sales, Person)
-			("0","852399038","22-08-2018","0","1","3"),
-			("1","731166526","31-12-2018","3","0","5"),
-			("2","271135104","22-01-2019","2","2","7");
+
+INSERT INTO customers (id_customer, name, phone_number, email, address, city, state_province, country, postal_code)
+VALUES
+    (10001, 'Pablo Picasso', '+34 636 17 63 82', NULL, 'Paseo de la Chopera, 14', 'Madrid', 'Madrid', 'Spain', '28045'),
+    (20001, 'Hedy Lamarr', '+43 1 514442250', NULL, 'Weiglgasse 10', 'Vienna', 'Vienna', 'Austria', '1150'),
+    (30001, 'Katherine Johnson', '+1 202 358 0000 300', NULL, 'E St SW', 'Washington', 'DC', 'United States', '20546');
+
+
+INSERT INTO salespersons (id_salesperson, name, store_location)
+VALUES
+    ('00001', 'Petey Cruiser', 'Madrid'),
+    ('00002', 'Anna Sthesia', 'Barcelona'),
+    ('00003', 'Paul Molive', 'Berlin'),
+    ('00004', 'Gail Forcewind', 'Paris');
+            
+INSERT INTO invoices (id_invoice, invoice_number, date, id_car, Salespersons_id_salesperson, Customers_id_customer, Cars_id_car)
+VALUES
+     (0, '852399038', '2018-08-22', "3K096I98581DHSNUP", '10001', '3', '1'),
+     (1, '731166526', '2018-12-31', "ZM8G7BEUQZ97IH46V", '20001', '5', '0'),
+     (2, '271135104', '2019-01-22', "RKXVNNIHLVVZOUB4M", '30001', '7', '2');
+
